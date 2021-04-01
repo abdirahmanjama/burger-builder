bedb649e8b08772106bb33dd4a7cb0b962d09c20
Abdirahman <abdirahman.jamaaa@gmail.com>
2021-04-01 22:44:02 +0100

    initial commit

---
+++ src/components/layout/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const layout = (props) => {
+  return (
+    <React.Fragment>
+      <div>Toolbar, Sidebar, Backdrop</div>
+
+      <main>{props.children}</main>
+    </React.Fragment>
+  );
+};
+
+export default layout;
